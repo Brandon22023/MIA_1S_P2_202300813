a0ac9d5609ab0ff58256099a7a0e491b99636bd6
Brandon Antonio Marroquin Pérez <3045062060114@ingenieria.usac.edu.gt>
2025-05-04 15:56:19 -0600

    no dire nada

--- Proyecto1/src/environments/environment.ts
+++ Proyecto1/src/environments/environment.ts
@@ -1,5 +1,5 @@
 
-const ANGULAR_PUBLIC_API_URL = 'http://localhost:3000';
+const ANGULAR_PUBLIC_API_URL = 'http://3.91.223.90:3000';
 export const environment = {
   production: false,
   apiUrl: ANGULAR_PUBLIC_API_URL || 'http://localhost:3000'
